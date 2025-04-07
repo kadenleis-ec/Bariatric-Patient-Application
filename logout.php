b77acd217c45d825d2dc59e8e75fdd76d7628f91
Pixedz <leiskj7536@uwec.edu>
2025-04-06 23:02:39 -0500

    3rd database

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start(); // start the session so we can clear it
+
+session_unset(); // remove all session variables
+session_destroy(); // end the session completely
+
+// send user back to the login page
+header("Location: login/log_in.html");
+exit;
+?>
